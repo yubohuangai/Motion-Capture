ee2f38740a188b10072b21fdcf2db298b6779d43
yubohuangai <yubo.huang@hotmail.com>
2025-12-11 02:16:08 -0700

    test  mmpose

--- easymocap/estimator/mmpose_wrapper.py
+++ easymocap/estimator/mmpose_wrapper.py
@@ -14,46 +14,35 @@ import torch
 
 COCO17_IN_BODY25 = [0,16,15,18,17,5,2,6,3,7,4,12,9,13,10,14,11]
 pairs = [[1, 8], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [8, 9], [9, 10], [10, 11], [8, 12], [12, 13], [13, 14], [1, 0], [0,15], [15,17], [0,16], [16,18], [14,19], [19,20], [14,21], [11,22], [22,23], [11,24]]
+
 def coco17tobody25(points2d):
+    """
+    Convert COCO17 (17x3) keypoints to BODY25 (25x3), preserving confidence.
+    Input: (N, 17, 3) -> x, y, score
+    Output: (N, 25, 3)
+    """
     kpts = np.zeros((points2d.shape[0], 25, 3))
+
+    # copy x, y
     kpts[:, COCO17_IN_BODY25, :2] = points2d[:, :, :2]
-    kpts[:, COCO17_IN_BODY25, 2:3] = points2d[:, :, 2:3]
-    # pelvis
+
+    # copy confidence
+    kpts[:, COCO17_IN_BODY25, 2] = points2d[:, :, 2]
+
+    # pelvis = midpoint(hip_l, hip_r)
     kpts[:, 8, :2] = kpts[:, [9, 12], :2].mean(axis=1)
-    kpts[:, 8, 2] = kpts[:, [9, 12], 2].min(axis=1)
-    # neck
+    kpts[:, 8, 2] = kpts[:, [9, 12], 2].min(axis=1)  # confidence: use min of hips
+
+    # neck = midpoint(shoulder_l, shoulder_r)
     kpts[:, 1, :2] = kpts[:, [2, 5], :2].mean(axis=1)
-    kpts[:, 1, 2] = kpts[:, [2, 5], 2].min(axis=1)
-    # 需要交换一下
-    # kpts = kpts[:, :, [1,0,2]]
+    kpts[:, 1, 2] = kpts[:, [2, 5], 2].min(axis=1)  # confidence: use min of shoulders
+
     return kpts
-
-
-def bbox_from_keypoints(keypoints, rescale=1.2, detection_thresh=0.05, MIN_PIXEL=5):
-    """Get center and scale for bounding box from openpose detections."""
-    valid = keypoints[:,-1] > detection_thresh
-    if valid.sum() < 3:
-        return [0, 0, 100, 100, 0]
-    valid_keypoints = keypoints[valid][:,:-1]
-    center = (valid_keypoints.max(axis=0) + valid_keypoints.min(axis=0))/2
-    bbox_size = valid_keypoints.max(axis=0) - valid_keypoints.min(axis=0)
-    # adjust bounding box tightness
-    if bbox_size[0] < MIN_PIXEL or bbox_size[1] < MIN_PIXEL:
-        return [0, 0, 100, 100, 0]
-    bbox_size = bbox_size * rescale
-    bbox = [
-        center[0] - bbox_size[0]/2, 
-        center[1] - bbox_size[1]/2,
-        center[0] + bbox_size[0]/2, 
-        center[1] + bbox_size[1]/2,
-        keypoints[valid, 2].mean()
-    ]
-    return bbox
 
 
 class MMPoseDetector:
 
-    def __init__(self, model_cfg='/mnt/yubo/mmpose/configs/body_2d_keypoint/rtmpose/coco/rtmpose-l_8xb256-420e_aic-coco-384x288.py', model_weights='data/models/rtmpose-l_simcc-aic-coco_pt-aic-coco_420e-384x288-97d6cb0f_20230228.pth', to_openpose=True):
+    def __init__(self, model_cfg, model_weights, to_openpose=True):
         """
         model_name: str, name of the MMPose model config (e.g., 'TopDownHRNet')
         to_openpose: whether to convert COCO17 keypoints to BODY25
@@ -66,23 +55,28 @@ class MMPoseDetector:
             device="cuda"
         )
         
-    def process_image(self, image):
+    def process(self, data, results, image_width, image_height):
         """Run MMPose inference on a single image"""
-        results = self.inferencer(image)
-        if len(results) == 0:
-            # no person detected
-            keypoints = np.zeros((1, 17, 3))  # COCO17
-        else:
-            # take the first person detected
-            keypoints = results[0]['keypoints']  # shape: (17, 3)
+        output = next(results)
+        persons = output['predictions'][0]
+        for pid, person in enumerate(persons):
+            kpts17 = np.array(person['keypoints'])
+            if 'keypoint_scores' in person:
+                conf = np.array(person['keypoint_scores'])
+            else:
+                conf = np.ones((kpts17.shape[0],), dtype=kpts17.dtype)
 
-        keypoints = keypoints[np.newaxis, ...]  # make batch dim
-        if self.to_openpose:
-            kpts25 = coco17tobody25(keypoints)
-        else:
-            kpts25 = keypoints
-        bbox = bbox_from_keypoints(kpts25[0])
-        return kpts25[0], bbox
+            if kpts17.shape[1] == 2:
+                kpts17 = np.concatenate([kpts17, conf[:,None]], axis=1)
+            else:
+                kpts17[:,2] = conf
+
+            kpts25 = coco17tobody25(kpts17[None])[0] 
+            bbox = person['bbox']
+
+            data['keypoints'] = kpts25.tolist()
+            data['bbox'] = bbox
+
 
     def __call__(self, images):
         """
@@ -90,15 +84,12 @@ class MMPoseDetector:
         returns: list of annotations dicts
         """
         annots_all = []
-        for nv, image_ in enumerate(images):
-            image_height, image_width, _ = image_.shape
-            image = cv2.cvtColor(image_, cv2.COLOR_BGR2RGB)
-            image.flags.writeable = False
-            
-            kpts, bbox = self.process_image(image_)
-            data = {
-                'personID': 0,
-            }
+        for nv, image in enumerate(images):
+            image_height, image_width, _ = image.shape
+            results = self.inferencer(image)
+            data = {}
+            self.process(data, results, image_width, image_height)
+
             annots = {
                 'filename': '{}/run.jpg'.format(nv),
                 'height': image_height,
@@ -113,13 +104,11 @@ class MMPoseDetector:
 
 
 def extract_2d(image_root, annot_root, config, to_openpose=True):
-    """Extract 2D keypoints for all images in a folder"""
-    if check_result(image_root, annot_root):
+    force = config.pop('force')
+    if check_result(image_root, annot_root) and not force:
         return 0
-    device = torch.device('cuda') \
-            if torch.cuda.is_available() else torch.device('cpu')
     ext = config.pop('ext')
-    detector = MMPoseDetector()
+    detector = MMPoseDetector(model_cfg=config['pose2d'], model_weights=config['pose2d_weights'], to_openpose=to_openpose)
     imgnames = sorted(glob(join(image_root, '*'+ext)))
     for imgname in tqdm(imgnames, desc='{:10s}'.format(os.path.basename(annot_root))):
         base = os.path.basename(imgname).replace(ext, '')
