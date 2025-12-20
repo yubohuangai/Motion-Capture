39d91567912549c0d2114c86d8501c4522e6032c
yubohuangai <yubo.huang@hotmail.com>
2025-12-20 08:43:37 -0700

    test edpose

--- apps/preprocess/extract_keypoints.py
+++ apps/preprocess/extract_keypoints.py
@@ -84,10 +84,13 @@ config = {
         'ext': '.jpg',
         # 'pose2d': '/mnt/yubo/mmpose/configs/body_2d_keypoint/rtmpose/coco/rtmpose-l_8xb256-420e_aic-coco-384x288.py',
         # 'pose2d_weights': 'data/models/rtmpose-l_simcc-aic-coco_pt-aic-coco_420e-384x288-97d6cb0f_20230228.pth',
-        #
-        'pose2d': '/mnt/yubo/mmpose/configs/body_2d_keypoint/edpose/coco/edpose_res50_8xb2-50e_coco-800x1333.py',
-        'pose2d_weights': 'data/models/edpose_res50_coco_3rdparty.pth',
-        'config_name': 'edpose_res50_8xb2-50e_coco-800x1333'
+
+        # 'pose2d': '/mnt/yubo/mmpose/configs/body_2d_keypoint/edpose/coco/edpose_res50_8xb2-50e_coco-800x1333.py',
+        # 'pose2d_weights': 'data/models/edpose_res50_coco_3rdparty.pth',
+        # 'config_name': 'edpose_res50_8xb2-50e_coco-800x1333'
+
+        'pose2d': '/mnt/yubo/mmpose/configs/body_2d_keypoint/simcc/coco/simcc_vipnas-mbv3_8xb64-210e_coco-256x192.py',
+        'pose2d_weights': 'data/models/simcc_vipnas-mbv3_8xb64-210e_coco-256x192-719f3489_20220922.pth',
     }
 }
 
